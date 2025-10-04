a212f93c35d82d1508a01797c03c172840bda031
mukeshteli <mokshh8301@gmail.com>
2025-10-04 17:22:45 +0530

    changes into db file

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";  
-$password = "";      
-$dbname = "kolpek_db";
-
+$servername = "sql306.infinityfree.com";   // MySQL Host provided by InfinityFree
+$username   = "if0_40067450";              // MySQL User Name
+$password   = "ElrqwrpCXXMftR";    // Replace with your actual InfinityFree password
+$dbname     = "if0_40067450_kolpek_db";    // MySQL Database Name
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
